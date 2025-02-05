f20b01c39e9fdfef4db52d3e441a67be91d05c70
Hamin Park <haeeema@gmail.com>
2025-02-04 23:40:18 -0600

    String

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("I'm Hamin!");
+
+string firstFriends = "Nicole";
+string secondFriends = "Ray";
+
+// Composite formatting:
+Console.WriteLine($"hello {firstFriends} and {secondFriends}");
+// String interpolation:
+Console.WriteLine("Hello " + firstFriends + " and " + secondFriends);
+
+string greeting = $"hello {firstFriends} and {secondFriends}";
+Console.WriteLine(greeting);
+greeting = greeting.Replace("Ray", "Donna");
+Console.WriteLine(greeting);
+
+
